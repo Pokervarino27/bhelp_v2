96496ca4f970e1b2b4aee79879cda5ce61fb751b
dcomihual <dcomihual@imaginex.cl>
2019-03-17 23:28:01 -0300

    update models

--- models.go
+++ models.go
@@ -2,33 +2,33 @@ package main
 
 //ErrorMsg estructura del Mensaje de ErrorMsg
 type ErrorMsg struct{
-  TrackId         int     `json:"trackId,omitempty"`
-  CpnId           int     `json:"cpnId,omitempty"`
-  TrackType       string  `json:"trackType,omitempty"`
-  CreateDate      int     `json:"createDate,omitempty"`
-  ResolutionDate  int     `json:"resolutionDate,omitempty"`
-  TrackState      int     `json:"trackState,omitempty"`
-  UsrId           int     `json:"usrId,omitempty"`
-  UsrName         string  `json:"usrName,omitempty"`
-  ticketId        int     `json:"ticketId,omitempty"`
-  TrackData
-  InstanceData
+  TrackId         int     `json:"trackId"`
+  CpnId           int     `json:"cpnId"`
+  TrackType       string  `json:"trackType"`
+  CreateDate      int     `json:"createDate"`
+  ResolutionDate  int     `json:"resolutionDate"`
+  TrackState      int     `json:"trackState"`
+  UsrId           int     `json:"usrId"`
+  UsrName         string  `json:"usrName"`
+  TicketId        int     `json:"ticketId"`
+  // TrackData
+  // InstanceData
 }
 
 //TrackData estructura
 type TrackData struct{
-  Msg string `json: "msg,omitempty"`
+  Msg string `json: "msg"`
 }
 
 //InstanceData estructura
 type InstanceData struct{
-  CpnName         string `json:"cpnName,omitempty"`
-  LegalName       string `json:"legalName,omitempty"`
-  CpnCode         string `json:"cpnCode,omitempty"`
-  ContactName     string `json:"contactName,omitempty"`
-  ContactEmail    string `json:"contactEmail,omitempty"`
-  ContactPhone    string `json:"contactPhone,omitempty"`
-  LegalAgentName  string `json:"legalAgentName,omitempty"`
-  LegalAgentCode  string `json:"legalAgentCode,omitempty"`
-  LegalAgentEmail string `json:"legalAgentEmail,omitempty"`
+  CpnName         string `json:"cpnName"`
+  LegalName       string `json:"legalName"`
+  CpnCode         string `json:"cpnCode"`
+  ContactName     string `json:"contactName"`
+  ContactEmail    string `json:"contactEmail"`
+  ContactPhone    string `json:"contactPhone"`
+  LegalAgentName  string `json:"legalAgentName"`
+  LegalAgentCode  string `json:"legalAgentCode"`
+  LegalAgentEmail string `json:"legalAgentEmail"`
 }
